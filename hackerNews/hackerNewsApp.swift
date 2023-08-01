d4a15c610ad8d6a267e2e38004a1a6b1b2a3a6f2
matthewjljackson <matthewjljackson@gmail.com>
2023-08-01 20:04:27 +0100

    Initial Commit

---
+++ hackerNews/hackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackerNewsApp.swift
+//  hackerNews
+//
+//  Created by Matthew Jackson on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct hackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
